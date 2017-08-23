b9e3bcbda7121c7bece1bdcc5a3ce9a99f8d0f30
heui <runphp@qq.com>
2017-08-23 14:49:34 +0000

    Apply fixes from StyleCI

--- src/Di/InjectionAwareInterface.php
+++ src/Di/InjectionAwareInterface.php
@@ -17,12 +17,14 @@ use Phalcon\Di\InjectionAwareInterface as DiInjectionAwareInterface;
 
 /**
  * @property \Phalcon\Cache\Backend $cache
- * @property \Phalcon\Config $config 系统配置
+ * @property \Phalcon\Config $config 系统�
+ * �置
  * @property \Eelly\SDK\EellyClient $eellyClient
  * @property \Eelly\Dispatcher\EventDispatcher $eventDispatcher
  * @property \Eelly\FastDFS\Client $fastdfs fastdfs
  * @property \Phalcon\Loader $loader
- * @property \Phalcon\Config $moduleConfig 模块配置
+ * @property \Phalcon\Config $moduleConfig 模块�
+ * �置
  * @property \Psr\Log\LoggerInterface $logger 日志对象
  * @property \Eelly\Queue\Adapter\AMQPFactory|\Eelly\Queue\QueueFactoryInterface $queueFactory
  *
