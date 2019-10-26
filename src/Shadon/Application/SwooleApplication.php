e617588c553eaffa75d1ff61a318d5c126de6d4b
runphp <runphp@qq.com>
2019-10-27 04:17:10 +0800

    add "enqueue/amqp-lib": "^0.9.14", add swoole app

---
+++ src/Shadon/Application/SwooleApplication.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of eelly package.
+ *
+ * (c) eelly.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Shadon\Application;
+
+/**
+ * SwooleApplication run in php swoole.
+ *
+ * @author hehui<runphp@qq.com>
+ */
+class SwooleApplication
+{
+}
